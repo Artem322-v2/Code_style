c6ae43affd03acdbdf2c9d7fe54db1f1e0846c59
Artem322-v2 <Zakiev2100@mail.ru>
2025-06-23 20:38:05 +0400

    First coomit

---
+++ script.js
@@ -0,0 +1,12 @@
+function multiply (a , b) {
+  return a * b;
+}
+
+const person = {name: 'Alice' , age: 30};
+
+if (person.age > 18) {
+    console.log('Adult'); 
+}
+else {
+    console.log('Minor');
+}
